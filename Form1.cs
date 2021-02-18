ef9f7dbecaaff171aa0f57967587e0c9f33d190a
thomas-rooty <essainono1@gmail.com>
2021-02-18 14:30:48 +0100

    Class Utilisateur

--- Form1.cs
+++ Form1.cs
@@ -10,6 +10,17 @@ using System.Windows.Forms;
 
 namespace VAdmin
 {
+    public class Utilisateur
+    {
+        public string users_id { get; set; }
+        public string users_name { get; set; }
+        public string users_address { get; set; }
+        public string users_email { get; set; }
+        public string users_password { get; set; }
+        public string users_birthday { get; set; }
+        public string user_group { get; set; }
+        public string avatar_url { get; set; }
+    }
     public partial class Form1 : Form
     {
         public Form1()
